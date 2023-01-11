6f19536bcfeefd05b96bb23735b3929571534d01
Ferdjani <ferdjani.abdelkaderzakaria@gmail.com>
2023-01-11 13:05:11 +0100

    added metatag and customized the  components

---
+++ capstone8/src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <img src="#" alt="logo" />
+            <div>
+                <div>
+                    <h1>Doormat Navigation</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Menu</a>
+                        </li>
+                        <li>
+                            <a href="#">Reservations</a>
+                        </li>
+                        <li>
+                            <a href="#">Order Online</a>
+                        </li>
+                        <li>
+                            <a href="#">Login</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Contact</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Adress</a>
+                        </li>
+                        <li>
+                            <a href="#">Phone number</a>
+                        </li>
+                        <li>
+                            <a href="#">Email</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Social media Links</h1>
+                    <ul>
+                        <li>
+                            <a href="#">Adress</a>
+                        </li>
+                        <li>
+                            <a href="#">Phone number</a>
+                        </li>
+                        <li>
+                            <a href="#">Email</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
